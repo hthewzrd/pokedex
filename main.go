642d1f471f86cd8581ba8c2ff36e1a9f8bc63745
hthewzrd <dhund.data@gmail.com>
2026-02-28 20:41:26 +0000

    pokedex locations setup

--- main.go
+++ main.go
@@ -1,31 +1,16 @@
 package main
 
-import ("fmt"
-        "os"
-        "strings"
-        "bufio"
+import (
+	"time"
+
+	"github.com/bootdotdev/pokedexcli/internal/pokeapi"
 )
 
 func main() {
-    reader := bufio.NewScanner(os.Stdin)
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
 
-    for { 
-        fmt.Print("Pokedex > ")
-        success := reader.Scan()
-        
-        if !success {
-            break
-        }
-        text := reader.Text()
-        lowered := strings.ToLower(text)
-        words := strings.Fields(lowered)
-
-        if len(words) == 0 {
-            continue 
-        }
-
-        fmt.Printf("Your command was: %s\n", words[0])
-    }
-
+	startRepl(cfg)
 }
-
